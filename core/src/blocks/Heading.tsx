c1136eee98a1a5bab4a3cfeafd08f848635eabea
Aman-Kaushik13 <amanresearcher12@gmail.com>
2021-06-02 11:25:11 +0530

    Adding SVG Icons

--- core/src/blocks/Heading.tsx
+++ core/src/blocks/Heading.tsx
@@ -4,15 +4,75 @@ import { HeadingComponent } from "components/Heading/Heading.component";
 import { Block } from "types/block.types";
 
 const HeadingOneIcon: React.FC = () => (
-  <span><strong>H1</strong></span>
+  <svg
+  width={17}
+  height={14}
+  viewBox="0 0 17 14"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M1.063.918v5.799m0 5.798V6.717m0 0h8.764m0 0V.918m0 5.799v5.798"
+    stroke="#272727"
+    strokeWidth={1.8}
+    strokeLinecap="round"
+  />
+  <path
+    d="M14.128 7.821l2.132-1.104v6.201"
+    stroke="#272727"
+    strokeWidth={1.4}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />
+</svg>
 )
 
 const HeadingTwoIcon: React.FC = () => (
-  <span><strong>H2</strong></span>
+  <svg
+  width={19}
+  height={14}
+  viewBox="0 0 19 14"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M1.667.918v5.799m0 5.798V6.717m0 0h8.764m0 0V.918m0 5.799v5.798"
+    stroke="#272727"
+    strokeWidth={1.8}
+    strokeLinecap="round"
+  />
+  <path
+    d="M13.811 8.213c0-.78.97-1.496 2.018-1.496 1.049 0 2.3 1.22 1.585 2.571-.715 1.351-3.361 3.63-3.361 3.63h3.603"
+    stroke="#272727"
+    strokeWidth={1.4}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />
+</svg>
 )
 
 const HeadingThreeIcon: React.FC = () => (
-  <span><strong>H3</strong></span>
+  <svg
+  width={17}
+  height={14}
+  viewBox="0 0 17 14"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M1.46.918v5.718m0 5.718V6.636m0 0h7.627m0 0V.918m0 5.718v5.718"
+    stroke="#272727"
+    strokeWidth={1.8}
+    strokeLinecap="round"
+  />
+  <path
+    d="M12.637 7.723c0-.522.622-1.253 1.52-1.253.9 0 1.475.626 1.475 1.462 0 .835-.437 1.644-1.612 1.644.599.044 1.842.522 1.842 1.645 0 .966-.599 1.697-1.704 1.697-1.267 0-1.751-1.097-1.751-1.436"
+    stroke="#272727"
+    strokeWidth={1.4}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />
+</svg>
 )
 
 export type HeadingTypes = "heading1" | "heading2" | "heading3";
--- core/src/blocks/List.tsx
+++ core/src/blocks/List.tsx
@@ -4,11 +4,51 @@ import { ListComponent } from "components/List/List.component";
 import { Block } from "types/block.types";
 
 const OrderedListIcon: React.FC = () => (
-  <span><strong>OL</strong></span>
+  <svg
+  width={19}
+  height={14}
+  viewBox="0 0 19 14"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    opacity={0.7}
+    d="M7.195 1.463h10M7.195 7.213h10M7.195 12.963h10"
+    stroke="#272727"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />
+  <path
+    d="M3.517.012H1.903L.127 1.116v1.505l1.61-.984h.043v5.577h1.737V.012z"
+    fill="#272727"
+  />
+</svg>
 )
 
 const UnorderedListIcon: React.FC = () => (
-  <span><strong>UL</strong></span>
+  <svg
+  width={19}
+  height={16}
+  viewBox="0 0 19 16"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    opacity={0.7}
+    d="M7.214 2.512h10M7.214 8.262h10M7.214 14.012h10"
+    stroke="#272727"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />
+  <path
+    fillRule="evenodd"
+    clipRule="evenodd"
+    d="M.109 2.992c0-1.145.929-2.074 2.074-2.074h.008a2.074 2.074 0 110 4.149h-.008A2.074 2.074 0 01.11 2.992z"
+    fill="#272727"
+  />
+</svg>
 )
 
 export type ListTypes = "unorderedList" | "orderedList";
